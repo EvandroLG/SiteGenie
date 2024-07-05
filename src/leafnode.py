e045040527cae05053eed66f98108461b305abab
Evandro Leopoldino Goncalves <evandrolgoncalves@gmail.com>
2024-07-05 11:49:52 +0200

    implements method to convert from textnode to html node

--- src/leafnode.py
+++ src/leafnode.py
@@ -5,6 +5,7 @@ class LeafNode(HTMLNode):
     def __init__(self, tag=None, value=None, props=None):
         super().__init__(tag, value, None, props)
 
+    # TODO: handle case of image and when node has no tag
     def to_html(self):
         props_html = f" {self.props_to_html()}" if self.props else ""
         return f"<{self.tag}{props_html}>{self.value}</{self.tag}>"
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -8,5 +8,18 @@ class TestTextNode(unittest.TestCase):
         node_2 = TextNode("Hello", "Greeting", "https://www.example.com")
         self.assertEqual(node_1, node_2)
 
+    def test_to_html_node(self):
+        link = TextNode("Hello", "link", "https://www.example.com")
+        self.assertEqual(link.to_html_node().to_html(), '<a href="https://www.example.com">Hello</a>')
+
+        code = TextNode("print('Hello, World!')", "code", None)
+        self.assertEqual(code.to_html_node().to_html(), '<code>print(\'Hello, World!\')</code>')
+
+        bold = TextNode("Hello", "bold", None)
+        self.assertEqual(bold.to_html_node().to_html(), '<b>Hello</b>')
+
+        italic = TextNode("Hello", "italic", None)
+        self.assertEqual(italic.to_html_node().to_html(), '<i>Hello</i>')
+
 if __name__ == "__main__":
     unittest.main()
