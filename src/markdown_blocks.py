2e190efba92b5da526840da053f6cb8f7f03ab34
Evandro Leopoldino Goncalves <evandrolgoncalves@gmail.com>
2024-07-10 15:53:08 +0200

    implements logic to convert markdown to html node

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,5 +1,7 @@
 import re
 
+from htmlnode import HTMLNode
+
 
 block_type_paragraph = "paragraph"
 block_type_heading = "heading"
@@ -24,7 +26,7 @@ def markdown_to_block(markdown):
 def block_to_block_type(block):
     if re.match(r"^\s*#{1,6}\s", block):
         return block_type_heading
-    
+
     if block.startswith("```") and block.endswith("```"):
         return block_type_code
 
@@ -38,3 +40,45 @@ def block_to_block_type(block):
         return block_type_ordered_list
 
     return block_type_paragraph
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_block(markdown)
+    root = HTMLNode(tag="div", children=[])
+    i = 0
+
+    while i < len(blocks):
+        block = blocks[i]
+        block_type = block_to_block_type(block)
+
+        if block_type == block_type_heading:
+            heading_level = len(re.match(r"^\s*#{1,6}\s", block).group(0).strip())
+            block_node = HTMLNode(tag=f"h{heading_level}", value=re.sub(r"^\s*#{1,6}\s", "", block))
+        elif block_type == block_type_code:
+            block_node = HTMLNode(tag="code", children=[HTMLNode(tag="pre", value=re.sub(r"```", "", block))])
+        elif block_type == block_type_quota:
+            block_node = HTMLNode(tag="blockquote", value=re.sub(r"^>\s", "", block))
+        elif block_type == block_type_unordered_list:
+            block_node = HTMLNode(tag="ul", children=[])
+
+            while i < len(blocks) and block_to_block_type(blocks[i]) == block_type_unordered_list:
+                line = blocks[i]
+                block_node.children.append(HTMLNode(tag="li", value=re.sub(r"^[*-]\s", "", line)))
+                i += 1
+
+            i -= 1
+        elif block_type == block_type_ordered_list:
+            block_node = HTMLNode(tag="ol", children=[])
+
+            while i < len(blocks) and block_to_block_type(blocks[i]) == block_type_ordered_list:
+                line = blocks[i]
+                block_node.children.append(HTMLNode(tag="li", value=re.sub(r"^\d+\.\s", "", line)))
+                i += 1
+
+            i -= 1
+        else:
+            block_node = HTMLNode(tag="p", value=block)
+
+        i += 1
+        root.children.append(block_node)
+
+    return root
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -8,7 +8,8 @@ from markdown_blocks import (
     block_type_quota,
     block_type_unordered_list,
     block_type_ordered_list,
-    block_type_paragraph
+    block_type_paragraph,
+    markdown_to_html_node
 )
 
 class TestMarkdownBlocks(unittest.TestCase):
@@ -81,3 +82,54 @@ class TestMarkdownBlocks(unittest.TestCase):
     def test_block_type_paragraph(self):
         block = "This is a paragraph"
         self.assertEqual(block_to_block_type(block), block_type_paragraph)
+
+    def test_markdown_to_html_node(self):
+        markdown = """# This is a heading
+
+        This is a paragraph
+
+        ```print("Hello, World!")```
+
+        > This is a quota
+
+        * This is a list
+        * with items
+
+        1. This is a list
+        2. with items"""
+
+        html_node = markdown_to_html_node(markdown)
+
+        self.assertEqual(html_node.tag, "div")
+        self.assertEqual(len(html_node.children), 6)
+
+        self.assertEqual(html_node.children[0].tag, "h1")
+        self.assertEqual(html_node.children[0].value, "This is a heading")
+
+        self.assertEqual(html_node.children[1].tag, "p")
+        self.assertEqual(html_node.children[1].value, "This is a paragraph")
+
+        self.assertEqual(html_node.children[2].tag, "code")
+        self.assertEqual(html_node.children[2].children[0].tag, "pre")
+        self.assertEqual(html_node.children[2].children[0].value, "print(\"Hello, World!\")")
+
+        self.assertEqual(html_node.children[3].tag, "blockquote")
+        self.assertEqual(html_node.children[3].value, "This is a quota")
+
+        self.assertEqual(html_node.children[4].tag, "ul")
+        self.assertEqual(len(html_node.children[4].children), 2)
+
+        self.assertEqual(html_node.children[4].children[0].tag, "li")
+        self.assertEqual(html_node.children[4].children[0].value, "This is a list")
+
+        self.assertEqual(html_node.children[4].children[1].tag, "li")
+        self.assertEqual(html_node.children[4].children[1].value, "with items")
+
+        self.assertEqual(html_node.children[5].tag, "ol")
+        self.assertEqual(len(html_node.children[5].children), 2)
+
+        self.assertEqual(html_node.children[5].children[0].tag, "li")
+        self.assertEqual(html_node.children[5].children[0].value, "This is a list")
+
+        self.assertEqual(html_node.children[5].children[1].tag, "li")
+        self.assertEqual(html_node.children[5].children[1].value, "with items")
