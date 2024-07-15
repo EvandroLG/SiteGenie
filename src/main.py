9de8495396ea008f0320b1daa92a477fa2c2c580
Evandro Leopoldino Goncalves <evandrolgoncalves@gmail.com>
2024-07-15 21:08:54 +0200

    implements the main logic to generate all pages

--- src/main.py
+++ src/main.py
@@ -1,12 +1,45 @@
 import os
 import shutil
-from textnode import TextNode
+import logging
+
+from inline_markdown import extract_title
+from io_utils import read_file
+from markdown_blocks import markdown_to_html_node
 
 def main():
-    text_node = TextNode("Hello", "Greeting", "https://www.example.com")
-    print(text_node)
+    copy_files("static", "public")
+    generate_pages_from_directory("content", "template.html", "public")
 
-def copyFiles(source, target):
+def generate_pages_from_directory(directory, template_path, target_directory):
+    for file in os.listdir(directory):
+        if os.path.isdir(os.path.join(directory, file)):
+            generate_pages_from_directory(os.path.join(directory, file), template_path, os.path.join(target_directory, file))
+        elif file.endswith(".md"):
+            from_path = os.path.join(directory, file)
+            generate_page(from_path, template_path, target_directory)
+
+def generate_page(from_path, template_path, to_path):
+    print(f"Generating page from {from_path} to {to_path} using {template_path}")
+
+    markdown = read_file(from_path)
+    html = markdown_to_html_node(markdown).to_html()
+    title = extract_title(markdown)
+    template = (read_file(template_path)
+                    .replace("{{ Title }}", title)
+                    .replace("{{ Content }}", html))
+
+    if not os.path.isdir(to_path):
+        os.makedirs(to_path)
+
+
+    file = os.path.join(to_path, from_path.split("/")[-1]).replace(".md", ".html")
+
+    with open(file, "w") as file:
+        file.write(template)
+
+def copy_files(source, target):
+    logging.info(f"Copying files from {source} to {target}")
+
     shutil.rmtree(target, ignore_errors=True)
 
     def helper(src, dst):
@@ -25,5 +58,4 @@ def copyFiles(source, target):
     helper(source, target)
 
 if __name__ == "__main__":
-    # main()
-    copyFiles("static", "public")
+    main()
